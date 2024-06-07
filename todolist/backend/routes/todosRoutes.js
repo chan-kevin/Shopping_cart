d37f0fc187865a96bec1ffb534f300256043bfdf
Kevin Chan <chankevin13@gmail.com>
2024-06-07 00:53:45 -0400

    completed todos

--- todolist/backend/routes/todosRoutes.js
+++ todolist/backend/routes/todosRoutes.js
@@ -6,14 +6,28 @@ const jsonRouter = jsonServer.router("./db/db.json");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const data = jsonRouter.db.getState();
-  res.json(data.todos);
+  const data = jsonRouter.db.get("todos").value();
+  res.json(data);
 });
 
 router.post("/", (req, res) => {
   const newTodo = { id: uuid.v4(), ...req.body };
   jsonRouter.db.get("todos").push(newTodo).write();
   res.json(newTodo);
+});
+
+router.patch("/:id", (req, res) => {
+  const todoId = req.params.id;
+  const todo = jsonRouter.db.get("todos").find({ id: todoId }).value();
+  todo.content = req.body.content;
+  res.json(todo);
+});
+
+router.delete("/:id", (req, res) => {
+  const todoId = req.params.id;
+  console.log(todoId);
+  jsonRouter.db.get("todos").remove({ id: todoId }).write();
+  res.json({ id: todoId });
 });
 
 module.exports = router;
